288e6607dab16b365e008ab2707615d998a4461b
bnbln <benediktschnupp@me.com>
2022-06-04 20:11:11 +0200

    lottie

--- src/components/hero.js
+++ src/components/hero.js
@@ -1,6 +1,7 @@
 import { graphql } from "gatsby"
 import { GatsbyImage, getImage } from "gatsby-plugin-image"
 import * as React from "react"
+import { Player, Controls } from '@lottiefiles/react-lottie-player';
 import {
   Box,
   ButtonList,
@@ -12,18 +13,27 @@ import {
   Subhead,
   Text,
 } from "./ui"
+import animationData from "./Hero.json";
 
 export default function Hero(props) {
   return (
     <Section>
       <Container>
         <Flex gap={4} variant="responsive">
-          <Box width="half">
+          <Box width="half" style={{position: "relative", zIndex: 0}}>
             {props.image && (
+              <Player
+              autoplay
+              loop
+              src={animationData}
+              style={{ position: "absolute", top: 0, left: 0, width: "100%", height: "100%", zIndex: 20 }}
+            >
+              <Controls visible={false} buttons={['play', 'repeat', 'frame', 'debug']} />
               <GatsbyImage
                 alt={props.image.alt}
                 image={getImage(props.image.gatsbyImageData)}
               />
+            </Player>
             )}
           </Box>
           <Box width="half">
